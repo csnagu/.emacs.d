405c971058a4fab8db6319082c39edf4d39b4346
nagu <nagulog@gmail.com>
2014-03-24 16:12:16 +0900

    don't make backup files

--- init.el
+++ init.el
@@ -1,14 +1,15 @@
-;;
-;;slime
-;;
+;; バックアップファイルを作らないようにする
+(setq make-backup-files nil)
+;;; 終了時にオートセーブファイルを消す
+(setq delete-auto-save-files t)
+
+;; slime
 (add-to-list 'load-path "~/.emacs.d/slime")
 (setq inferior-lisp-program "/bin/sbcl")
 (require 'slime-autoloads)
 (slime-setup '(slime-repl))
 
-;;
-;;現在行のハイライト
-;;
+;; 現在行のハイライト
 (defface hlline-face
   '((((class color)
       (background dark))
@@ -22,14 +23,10 @@
 (setq hl-line-face 'hlline-face)
 (global-hl-line-mode)
 
-;;
-;;大文字小文字の区別をしない
-;;
+;; 大文字小文字の区別をしない
 (setq completion-ignore-case t)
 
-;;
-;;processing
-;;
+;; processing
 (add-to-list 'load-path "/home/nagu/.emacs.d/processing2-emacs/")
 (autoload 'processing-mode "processing-mode" "Processing mode" t)
 (add-to-list 'auto-mode-alist '("\\.pde$" . processing-mode))
@@ -38,17 +35,13 @@
 (setq processing-application-dir "/opt/processing-2.1.1")
 (setq processing-sketch-dir "/home/nagu/sketchbook")
 
-;;
-;;auto-installのパッケージ追加
-;;
+;; auto-installのパッケージ追加
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-;;
-;;anything
-;;
+;; anything
 (require 'anything-startup)
 (global-set-key (kbd "C-x b") 'anything-for-files)
 (global-set-key (kbd "M-y") 'anything-show-kill-ring)
@@ -57,29 +50,21 @@
 (setq recentf-max-saved-items 30)       ;; 保存するファイルの数
 (setq kill-ring-max 100)                ;; kill-ring で保存される最大値
 
-;;
-;;auto-complete
-;;
+;; auto-complete
 (require 'auto-complete-config)
 (ac-config-default)
 
-;;
 ;; rainbow-mode
-;;
 (defun my-rainbow-mode-turn-on ()
    (rainbow-mode t))  
 (add-hook 'css-mode-hook 'my-rainbow-mode-turn-on)
 (add-hook 'html-mode-hook 'my-rainbow-mode-turn-on)
 
-;;
 ;; バッファ移動
-;;
 (setq windmove-wrap-around t)
 (windmove-default-keybindings)
 
-;;
 ;;expand +  multiple 
-;;
 (require 'expand-region)
 (require 'multiple-cursors)
 (require 'smartrep)
@@ -100,11 +85,8 @@
 (global-set-key (kbd "C-c o c") 'helm-open-github-from-commit)
 (global-set-key (kbd "C-c o i") 'helm-open-github-from-issues)
 
-
-;;
 ;;theme
-;;
 (add-to-list 'custom-theme-load-path
              (file-name-as-directory "~/.emacs.d/elisp/themes/"))
-;(load-theme 'charcoal-black t t)
-;(enable-theme 'charcoal-black)
+(load-theme 'charcoal-black t t)
+(enable-theme 'charcoal-black)
